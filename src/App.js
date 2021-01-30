a076f0877059d78b3791b0e4513442aaf9a317ce
nirch <nirh2@yahoo.com>
2021-01-30 12:12:34 +0200

    Project skeleton. Includes the following: - react-router + bootstrap - navbar + all pages + navigation - server connection + login - functional components - active user context - saving active user to local storage + logout - deployment - adding classnames to page and component roots - assistant font - default color #5d5d5d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import LoginPage from './pages/login/LoginPage'
+import CoursesPage from './pages/courses/CoursesPage'
+import CourseDetailsPage from './pages/courses/CourseDetailsPage'
+import UsersPage from './pages/users/UsersPage'
+import UserDetailsPage from './pages/users/UserDetailsPage'
+import HoursReportPage from './pages/hours/HoursReportPage'
+import HoursApprovePage from './pages/hours/HoursApprovePage'
+import ActiveUserContext from './shared/activeUserContext'
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [activeUser, setActiveUser] = useState(localStorage.activeUser ? JSON.parse(localStorage.activeUser) : null);
+
+  const handleLogin = (activeUser) => {
+    setActiveUser(activeUser);
+    localStorage.activeUser = JSON.stringify(activeUser);
+  }
+
+  const handleLogout = () => {
+    setActiveUser(null);
+    localStorage.removeItem("activeUser");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ActiveUserContext.Provider value={activeUser}>
+      <Switch>
+        <Route exact path="/">
+          <LoginPage handleLogin={handleLogin} />
+        </Route>
+        <Route exact path="/courses">
+          <CoursesPage handleLogout={handleLogout}/>
+        </Route>
+        <Route path="/courses/:id">
+          <CourseDetailsPage handleLogout={handleLogout}/>
+        </Route>
+        <Route exact path="/users">
+          <UsersPage handleLogout={handleLogout}/>
+        </Route>
+        <Route path="/users/:id">
+          <UserDetailsPage handleLogout={handleLogout}/>
+        </Route>
+        <Route path="/hours-report">
+          <HoursReportPage handleLogout={handleLogout}/>
+        </Route>
+        <Route path="/hours-approve">
+          <HoursApprovePage handleLogout={handleLogout}/>
+        </Route>
+      </Switch>
+    </ActiveUserContext.Provider>
   );
 }
 
