cca6263c8fe13bcc6bb03ce1f901dafda209da27
Nogalasman <noga.lasman@gmail.com>
2021-01-31 14:57:59 +0200

    The first button is selected by default

--- src/components/portalButtonSet/PortalButtonSet.js
+++ src/components/portalButtonSet/PortalButtonSet.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import './PortalButtonSet.css'
 
 function PortalButtonSet(props) {
 
     const { buttons } = props;
+    const [ selectedButton, setSelectedButton ] = useState(buttons[0]);
 
-    const buttonSetView = buttons.map(btn => <button key={btn.key} type="button">{btn.label}</button>);
+    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key} type="button">{btn.label}</button>);
 
     return (
         <div className="c-portal-button-set">
