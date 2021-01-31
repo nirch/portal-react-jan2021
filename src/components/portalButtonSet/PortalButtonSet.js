953e1d956d9e19fb4bd6e231df469ed513847154
Nogalasman <noga.lasman@gmail.com>
2021-01-31 15:41:20 +0200

    convert selectedButton from state to prop

--- src/components/portalButtonSet/PortalButtonSet.js
+++ src/components/portalButtonSet/PortalButtonSet.js
@@ -1,17 +1,10 @@
-import { useState } from 'react';
 import './PortalButtonSet.css'
 
 function PortalButtonSet(props) {
 
-    const { buttons, handleClick } = props;
-    const [ selectedButton, setSelectedButton ] = useState(buttons[0]);
+    const { buttons, handleClick, selectedButton } = props;
 
-    const onSelect = (btn) => { 
-        setSelectedButton(btn);
-        handleClick(btn);
-    };
-
-    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key} type="button" onClick={() => onSelect(btn)}>{btn.label}</button>);
+    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key} type="button" onClick={() => handleClick(btn)}>{btn.label}</button>);
 
     return (
         <div className="c-portal-button-set">
