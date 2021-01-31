4f1698cf29a752f0e581ebff4be22ad4493498e5
Nogalasman <noga.lasman@gmail.com>
2021-01-31 14:24:01 +0200

    PortalButtonSet skeleton

---
+++ src/components/portalButtonSet/PortalButtonSet.js
@@ -0,0 +1,9 @@
+import './PortalButtonSet.css'
+
+function PortalButtonSet(props) {
+    return(
+        <div className="c-portal-button-set">PortalButtonSet</div>
+    );
+}
+
+export default PortalButtonSet;
