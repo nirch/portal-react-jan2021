ff19f0712d3f70a0db4ee620946a93dc6182d117
Nogalasman <noga.lasman@gmail.com>
2021-01-31 15:18:43 +0200

    sends the object of the clicked button to parent

--- src/components/portalButtonSet/PortalButtonSet.js
+++ src/components/portalButtonSet/PortalButtonSet.js
@@ -3,10 +3,15 @@ import './PortalButtonSet.css'
 
 function PortalButtonSet(props) {
 
-    const { buttons } = props;
+    const { buttons, handleClick } = props;
     const [ selectedButton, setSelectedButton ] = useState(buttons[0]);
 
-    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key} type="button">{btn.label}</button>);
+    const onSelect = (btn) => { 
+        setSelectedButton(btn);
+        handleClick(btn);
+    };
+
+    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key} type="button" onClick={() => onSelect(btn)}>{btn.label}</button>);
 
     return (
         <div className="c-portal-button-set">
