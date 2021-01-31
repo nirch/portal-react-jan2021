3b26b2d1747f2deb8799642b0b1be7a09efc60b9
Nogalasman <noga.lasman@gmail.com>
2021-01-31 16:24:01 +0200

    add top border shadow option

--- src/components/portalButtonSet/PortalButtonSet.js
+++ src/components/portalButtonSet/PortalButtonSet.js
@@ -2,12 +2,13 @@ import './PortalButtonSet.css'
 
 function PortalButtonSet(props) {
 
-    const { buttons, handleClick, selectedButton } = props;
+    const { buttons, handleClick, selectedButton, hasTopBorder } = props;
 
-    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key} type="button" onClick={() => handleClick(btn)}>{btn.label}</button>);
+    const buttonSetView = buttons.map(btn => <button className={selectedButton.key === btn.key ? "selected" : ""} key={btn.key}
+        type="button" onClick={() => handleClick(btn)}>{btn.label}</button>);
 
     return (
-        <div className="c-portal-button-set">
+        <div className={"c-portal-button-set" + (hasTopBorder ? " hasTopBorder" : "")}>
             {buttonSetView}
         </div>
     );
