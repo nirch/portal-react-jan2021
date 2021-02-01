2e24d91421b32843cda05f7d0736d11a2ff9da76
Nogalasman <noga.lasman@gmail.com>
2021-02-01 23:34:20 +0200

    add month steps limit and convert arrows to images

--- src/components/portalDatePicker/PortalDatePicker.js
+++ src/components/portalDatePicker/PortalDatePicker.js
@@ -1,7 +1,10 @@
+
+import { useState } from 'react';
 import './PortalDatePicker.css';
 
 function PortalDatePicker(props) {
-    const { date, handleDateSelection, onlyMonth } = props;
+    const { date, handleDateSelection, onlyMonth, stepsLimit } = props;
+    const [ stepsCount, setStepsCount ] = useState(0);
 
     const Months=["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"];
 
@@ -18,8 +21,18 @@ function PortalDatePicker(props) {
             "month": newDate.getMonth() + 1,
             "year": newDate.getFullYear()
         }
+        setStepsCount(stepsCount + step);
 
         handleDateSelection(newDateObj);
+    }
+
+    const isDisabled = (nextStep) => {
+        if (onlyMonth) {
+
+           return ((Math.abs(stepsCount + nextStep)) > stepsLimit);
+        }
+
+        return false;
     }
 
     function pad(s) { return (s < 10) ? '0' + s : s; }
@@ -29,17 +42,18 @@ function PortalDatePicker(props) {
         const cDate = date ? new Date(date.year + "-" + date.month + "-" + date.day) : new Date();
 
         if (onlyMonth) {
-            return Months[cDate.getMonth()];
+            return Months[cDate.getMonth()] + " " + cDate.getFullYear();
         }
-        else {
-            return [pad(cDate.getDate()), pad(cDate.getMonth() + 1), cDate.getFullYear()].join('/');
-        }
+        
+        return [pad(cDate.getDate()), pad(cDate.getMonth() + 1), cDate.getFullYear()].join('/');
         
     }
 
     return (
         <div className="c-portal-date-picker">
-            <button type="button" className="button-next" onClick={() => handleClick(1)}>&lt;</button><div className="date-view">{dateView()}</div><button type="button" className="button-back" onClick={() => handleClick(-1)}>&gt;</button>
+            <button type="button" className="button-next" onClick={() => handleClick(1)} disabled={isDisabled(1)}></button>
+                <div className="date-view">{dateView()}</div>
+            <button type="button" className="button-back" onClick={() => handleClick(-1)} disabled={isDisabled(-1)}></button>
         </div>
     );
 }
