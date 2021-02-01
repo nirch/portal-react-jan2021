32a079b54e945f7e47c9745bd28f96ffa53a4a87
Nogalasman <noga.lasman@gmail.com>
2021-02-01 16:15:53 +0200

    add only month option

--- src/components/portalDatePicker/PortalDatePicker.js
+++ src/components/portalDatePicker/PortalDatePicker.js
@@ -1,11 +1,18 @@
 import './PortalDatePicker.css';
 
 function PortalDatePicker(props) {
-    const { date, handleDateSelection } = props;
+    const { date, handleDateSelection, onlyMonth } = props;
+
+    const Months=["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"];
 
     function handleClick(step) {
         let newDate = date ? new Date(date.year + "-" + date.month + "-" + date.day) : new Date();
-        newDate.setDate(newDate.getDate() + step);
+        if (onlyMonth) {
+            newDate.setMonth(newDate.getMonth() + step);
+        } else {
+            newDate.setDate(newDate.getDate() + step);
+        }
+
         const newDateObj = {
             "day": newDate.getDate(),
             "month": newDate.getMonth() + 1,
@@ -20,12 +27,19 @@ function PortalDatePicker(props) {
     const dateView = () => {
 
         const cDate = date ? new Date(date.year + "-" + date.month + "-" + date.day) : new Date();
-        return [pad(cDate.getDate()), pad(cDate.getMonth() + 1), cDate.getFullYear()].join('/');
+
+        if (onlyMonth) {
+            return Months[cDate.getMonth()];
+        }
+        else {
+            return [pad(cDate.getDate()), pad(cDate.getMonth() + 1), cDate.getFullYear()].join('/');
+        }
+        
     }
 
     return (
         <div className="c-portal-date-picker">
-            <button type="button" className="button-next" onClick={() => handleClick(1)}>&lt;</button>{dateView()}<button type="button" className="button-back" onClick={() => handleClick(-1)}>&gt;</button>
+            <button type="button" className="button-next" onClick={() => handleClick(1)}>&lt;</button><div className="date-view">{dateView()}</div><button type="button" className="button-back" onClick={() => handleClick(-1)}>&gt;</button>
         </div>
     );
 }
