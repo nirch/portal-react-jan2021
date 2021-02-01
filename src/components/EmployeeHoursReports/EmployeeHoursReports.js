fb47696bc8ae23c424556e9dc795b36b7ad90dca
Menachem Halperin <mendelhalp@gmail.com>
2021-02-01 13:51:24 +0200

    adding arrow img to report-header + animation

--- src/components/EmployeeHoursReports/EmployeeHoursReports.js
+++ src/components/EmployeeHoursReports/EmployeeHoursReports.js
@@ -1,9 +1,10 @@
 import './EmployeeHoursReports.css'
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Accordion } from 'react-bootstrap';
 import CustomToggle from './CustomToggle';
 import ReportingButtons from './ReportingButtons/ReportingButtons';
 import ReportDetails from './ReportDetails/ReportDetails';
+import arrow from '../../assets/images/arrow_left.png';
 
 // array of reports
 // calculate the hours
@@ -11,6 +12,7 @@ import ReportDetails from './ReportDetails/ReportDetails';
 const EmployeeHoursReports = ({reports}) => {
     const {firstname, lastname, approved, unapproved, rejected } = reports[0];
     const [isAllChecked, setIsAllChecked] = useState(false);
+    const [isOpen, setIsOpen] = useState('');
 
 
     function onStatusSelect(status){
@@ -19,9 +21,23 @@ const EmployeeHoursReports = ({reports}) => {
         // sending update request to the server
     }
 
+    const arrowAnimationStyle = useMemo(() => {
+        if (isOpen === '') {
+            return ''
+        } else if (isOpen) {
+            return 'spin-up'
+        } else {
+            return 'spin-down'
+        }
+    },[isOpen]);
+
+    function toggleClicked(){
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div className='c-employee-hours-reports'>
-            <CustomToggle eventKey="0">
+            <CustomToggle eventKey="0" toggleClicked={toggleClicked}>
                 <div className='employee-name'>{firstname} {lastname}</div>
                 <div className='total-hours'>
                     <div className='unapproved'>{unapproved}</div>
@@ -29,7 +45,7 @@ const EmployeeHoursReports = ({reports}) => {
                     <div className='rejected'>{rejected}</div>
                     <div className='total'>{unapproved+approved+rejected}</div>
                 </div>
-                <div className='arrow'></div>
+                <div className='arrow'><img className={arrowAnimationStyle} src={arrow}/></div>
             </CustomToggle>
             <Accordion.Collapse eventKey="0">
                 <div className='report-body'>
