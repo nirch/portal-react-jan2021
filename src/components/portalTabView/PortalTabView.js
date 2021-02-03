c0fa3360c9d690224c8347ce123a0c15b206e362
Nogalasman <noga.lasman@gmail.com>
2021-02-03 13:30:19 +0200

    hard coded tabs

--- src/components/portalTabView/PortalTabView.js
+++ src/components/portalTabView/PortalTabView.js
@@ -1,9 +1,23 @@
+import { Tab, Tabs } from 'react-bootstrap';
 import './PortalTabView.css';
 
-function PortalTabView(props){
-    <div className="c-portal-view">
+function PortalTabView(props) {
+    return (
+        <div className="c-portal-tab-view">
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                <Tab eventKey="home" title="פרופיל" className="tab-header">
+                    <div className="tab-content">פרופיל</div>
+                </Tab>
+                <Tab eventKey="profile" title="קורסים" className="tab-header">
+                    <div className="tab-content">קורסים</div>
+                </Tab>
+                <Tab eventKey="contact" title="עובדים" className="tab-header">
+                    <div className="tab-content">עובדים</div>
+                </Tab>
+            </Tabs>
+        </div>
+    );
 
-    </div>
 }
 
 export default PortalTabView;
