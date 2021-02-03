8b097e94118bd52af5dab9ec6c5d0d51cbd156db
Nogalasman <noga.lasman@gmail.com>
2021-02-03 16:52:04 +0200

    remove id from array object and use index instead

--- src/components/portalTabView/PortalTabView.js
+++ src/components/portalTabView/PortalTabView.js
@@ -5,13 +5,13 @@ function PortalTabView(props) {
 
     const { tabs } = props;
 
-    const tabsView = tabs.map(tab => <Tab key={tab.id} eventKey={tab.id} title={tab.header} className="tab-header">
+    const tabsView = tabs.map((tab, index) => <Tab key={index} eventKey={index} title={tab.header} className="tab-header">
         <div className="tab-content">{tab.view}</div>
     </Tab>)
 
     return (
         <div className="c-portal-tab-view">
-            <Tabs defaultActiveKey={tabs[0].id}>
+            <Tabs defaultActiveKey={0}>
                 {tabsView}
             </Tabs>
         </div>
