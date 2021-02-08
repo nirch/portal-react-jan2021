dc178635b43796b23fb9ebc716b6db80e37abb62
rev bar <revital.jer@gmail.com>
2021-02-08 12:43:34 +0200

    syllabus page

--- src/components/CourseDetails/CourseSyllabusTab/CourseSyllabusTab.js
+++ src/components/CourseDetails/CourseSyllabusTab/CourseSyllabusTab.js
@@ -1,6 +1,56 @@
+import { Accordion, Card ,Button} from "react-bootstrap";
 import "./CourseSyllabusTab.css"
+
 function CourseSyllabusTab(props)
 {
-    return (<div>סילבוס</div>);
+    
+    const {courseDetails} = props;
+    
+    let syllabus="";
+    let syllabusSub="";
+    let syllabusToPresent="";
+    if (courseDetails){
+
+        console.log(courseDetails);
+        console.log(courseDetails.subjects[0].subject);
+        syllabus= courseDetails.subjects;
+        console.log(syllabus);
+
+        syllabusToPresent= syllabus.map(function (syllabusSubject,index) {
+                            const subSubjects = syllabusSubject.subsubjects;
+                            console.log(subSubjects[0].subject);        
+                            const subSubjectsToPrint = subSubjects.map ((subSubject,index)=> <p key={index}>{subSubject.subject} </p>)
+                            console.log(subSubjectsToPrint);
+                            return (
+                                    <>
+                                       <Accordion defaultActiveKey="0">
+                                            <Card>
+                                                <Card.Header>
+                                                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                                        {syllabusSubject.subject}
+                                                    </Accordion.Toggle>
+                                                </Card.Header>
+                                                <Accordion.Collapse eventKey="0">
+                                                    <Card.Body>{subSubjectsToPrint}</Card.Body>
+                                                </Accordion.Collapse>
+                                            </Card>
+                                          
+                                            </Accordion>
+                                    </>
+                                        
+                                    
+                                    )
+                            }  )
+
+                            
+
+    }
+
+
+    return (
+        <div>
+            {syllabusToPresent}
+        </div>
+    );
 }
 export default CourseSyllabusTab;
